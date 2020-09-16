1af0ba8ff8a2c46e8e5d6d8514fecb074c15a404
areye022 <areye022@gmail.com>
2020-09-16 10:47:56 -0700

    created onclick to append searched city forecasts

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Define current coordinates (which will be loaded from navigator object).
+//these will be used to define our current location.
 var latitude; 
 var longitude; 
 
@@ -18,9 +18,6 @@ function locationRetrieved(position) {
     latitude = position.coords.latitude; 
     longitude = position.coords.longitude; 
 
-    //Add a button to the page with an event listener to pass in a request to the API using local coordinates.
-    //HERE
-
     //In the meantime, call the function that makes the reqest once the coordinates load. 
     requestCurrent(); 
 }
@@ -30,7 +27,7 @@ function requestCurrent(event) {
 
     //Set up the weather API request. 
     // var weatherAPI = 'ab855531e2a84587938c2262d8863c07'
-    var localQuery = 'http://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&APPID=ab855531e2a84587938c2262d8863c07'; 
+    var localQuery = 'http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&APPID=ab855531e2a84587938c2262d8863c07'; 
 
     //Make API call.
     $.ajax({
@@ -39,73 +36,97 @@ function requestCurrent(event) {
     }).then(function(response){
         console.log(response)
 
-        // onclick event for current location. Will append info only when clicked 
+        // onclick event for current location. Will append info to page only when clicked 
     $('#current').on('click',function(event){
         event.preventDefault();
 
-    for (results=0;results<5;results++){
-
         // to append current city name
     var currentWeather=document.querySelector('#current-weather')
-    var currentCity=document.createElement('h3');
-    currentCity.textContent=response.city.name;
+    var currentCity=document.createElement('h4');
+    currentCity.textContent=response.name;
     currentWeather.append(currentCity);
 
     // to append date
-    var date=document.createElement('h3');
-    date.textContent=response.list[results].dt_txt;
+    var date=document.createElement('h5');
+    date.textContent=response.dt;
     currentWeather.append(date);
 
     // to append current city weather
-    var main=document.createElement('h3');
-
-    // when i put results in here it shows as undefined, why?
-    main.textContent=response.list[0].weather[0].description;
+    var main=document.createElement('h5');
+    main.textContent='Weather Conditions: '+response.weather[0].main;
     currentWeather.append(main);
 
     // to append current city temp
-    var currentTemp=document.createElement('h3');
-    currentTemp.textContent=(response.list[results].main.temp - 273.15) * 1.8 + 32
+    var currentTemp=document.createElement('h5');
+    currentTemp.textContent='Temperature: '+(response.main.temp - 273.15) * 1.8 + 32
     currentWeather.append(currentTemp);
 
     // to append current humidity
-    var humidity=document.createElement('h3');
-    humidity.textContent='Humidity ' + response.list[results].main.humidity;
+    var humidity=document.createElement('h5');
+    humidity.textContent='Humidity: ' + response.main.humidity;
     currentWeather.append(humidity);
 
     // to append current windspeed
-    var windSpeed=document.createElement('h3');
-    windSpeed.textContent='Wind Speed '+ response.list[results].wind.speed;
+    var windSpeed=document.createElement('h5');
+    windSpeed.textContent='Wind Speed: '+ response.wind.speed;
     currentWeather.append(windSpeed);
-    }
+    
     // having issues finding uv index
 })
     })
 };
 
-// when we click on current location button, the local city name will append to the #current weather div
-$('#current').on('click',function(event){
-    event.preventDefault();
-        
-})
-
-
 // function to search a specific city
 function weatherSearch(){
 
     var search=$('#keyTerm').val();
-    
-    var searchQuery='http://api.openweathermap.org/data/2.5/forecast?q='+search+'&appid=ab855531e2a84587938c2262d8863c07'
+        
+    var searchQuery='http://api.openweathermap.org/data/2.5/weather?q='+search+'&appid=ab855531e2a84587938c2262d8863c07'
 
+    // API call
     $.ajax({
         url: searchQuery,
         method: "GET"
     }).then(function(res){
         console.log(res)
-})
-    console.log(search);
+
+        // to append city name
+        var searchWeather=document.querySelector('#searched-city');
+        searchCity=document.createElement('h4');
+        searchCity.textContent=res.name;
+        searchWeather.append(searchCity);
+
+    // to append date
+    // date doesn't come out correctly , might just use moment();
+        var sdate=document.createElement('h5');
+        sdate.textContent=res.dt;
+        searchWeather.append(sdate);
     
-}
+        // to append current city weather
+        var smain=document.createElement('h5');
+        smain.textContent='Weather Conditions: '+res.weather[0].main;
+        searchWeather.append(smain);
+    
+        // to append current city temp
+        var searchTemp=document.createElement('h5');
+        searchTemp.textContent='Temperature: '+(res.main.temp - 273.15) * 1.8 + 32
+        searchWeather.append(searchTemp);
+    
+        // to append current humidity
+        var shumidity=document.createElement('h5');
+        shumidity.textContent='Humidity: ' + res.main.humidity;
+        searchWeather.append(shumidity);
+    
+        // to append current windspeed
+        var swindSpeed=document.createElement('h5');
+        swindSpeed.textContent='Wind Speed: '+ res.wind.speed;
+        searchWeather.append(swindSpeed);
+        
+    
+    
+})  
+};
+
 // creating onclick for weathersearch function/search button
 $('#searchBtn').on('click',function(event){
     event.preventDefault();
