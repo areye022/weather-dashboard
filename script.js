79fdb3ff4fb5af54cbb67ac265d698fab73579eb
areye022 <areye022@gmail.com>
2020-09-17 14:47:37 -0700

    changed api, and am now able to show 5 day forecast instead of only current day

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // TODO
 // add forecast for following 5 days, currently only have current day loading
 // create local storage so the previous searches save
-// fix dates
 // clear input when searching current location, already did this for searched locations
 
 //these will be used to define our current location.
@@ -108,72 +107,75 @@ function requestCurrent(event) {
 function weatherSearch(){
 
     var search=$('#keyTerm').val();
-        
-    var searchQuery='http://api.openweathermap.org/data/2.5/weather?q='+search+'&appid=ab855531e2a84587938c2262d8863c07'
+    var searchQuery='http:api.openweathermap.org/data/2.5/forecast?q='+search+'&cnt=5&appid=ab855531e2a84587938c2262d8863c07'
+    // var searchQuery='http://api.openweathermap.org/data/2.5/weather?q='+search+'&appid=ab855531e2a84587938c2262d8863c07'
 
     // API call
     $.ajax({
         url: searchQuery,
         method: "GET"
-    }).then(function(res){
-        console.log(res)
+    }).then(function(res) {
+        for (var i=0;i<5;i++){
+            console.log(res);
 
         // to append city name
         var searchWeather=document.querySelector('#searched-city');
         searchCity=document.createElement('h4');
-        searchCity.textContent=res.name;
+        searchCity.textContent=res.city.name;
         searchWeather.append(searchCity);
 
-        // adding icons
-    if (res.weather[0].main==='Clouds'){
+    //     // adding icons
+    if (res.list[i].weather[0].main==='Clouds'){
         $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ3bjRqzFjxkmvAWL3ff72-tH8UidJ4UxFzmQ&usqp=CAU' height=200px width:200px />");
     }
-    else if(res.weather[0].main==='Haze'){
+    else if(res.list[i].weather[0].main==='haze'){
         $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ3bjRqzFjxkmvAWL3ff72-tH8UidJ4UxFzmQ&usqp=CAU' height=200px width:200px />");
     }
-    else if(res.weather[0].main==='Clear'){
+    else if(res.list[i].weather[0].main==='Clear'){
         $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQB5rA_3-rOSGZygBtaYfgA6YdfW9pRFav4vQ&usqp=CAU' height=200px width:200px />");
     }
-    else if(res.weather[0].main==='Rain'){
+    else if(res.list[i].weather[0].main==='Rain'){
         $("#image-div").html("<img src='https://lh3.googleusercontent.com/proxy/osGz8h9UkfY_pgBR60hlzNuBDfubrEzmOeipoz54xKwsrl900mZEQl8pru8LAWpLxpwlWaQUCJclxaj6oQZhkjA4Tx6RfGpXEJRwNV2Ug9PcAxFkZDmX7EOfyT5_t7UyhMTxSM8WgOLOfxc5tpf_adMnsp-D2KcocJyrFAk7V4_JFWQQPLnddvUKaRw' height=200px width:200px />");
     }
-    else if(res.weather[0].main==='Thunderstorm'){
+    else if(res.list[i].weather[0].main==='Thunderstorm'){
         $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHUzZMG5T41Na5OYewYjGrMwy56nxcyl1vJQ&usqp=CAU' height=200px width:200px />");
     }
-    else if(res.weather[0].main==='snow'){
+    else if(res.list[i].weather[0].main==='snow'){
         $("#image-div").html("<img src='https://lh3.googleusercontent.com/proxy/2ZPXMzmsBUygSnCxTujchS45Ry6QCRfhbt_xPGOKdxOpcMs_AMkMBKtG3Zblqgalyxu-vKRilAeQPkp4JOEPTYGMR0343H2DoqGr-nQU-tHhbIcmkQDzOnM-uBGDF1BqFklABBYkSgt3KTy_Mh0aW8KeNV91uVW9CTB9j41dcy0B5wr8LIXm0on7G0VmiJpqaPlk9Q' height=200px width:200px />");
     }
 
-    // to append date
+
+//     // to append date
     // date doesn't come out correctly , might just use moment();
-    var sdate = document.createElement("h5");
-    var snow = moment().format("MMMM Do YYYY");
+    var forecastDate = document.createElement("h5");
+    var forecastnow = moment().format("MMMM Do YYYY");
     // updating the text in the html
-    sdate.innerHTML = snow
-    searchWeather.append(sdate);
+    forecastDate.innerHTML = forecastnow
+    searchWeather.append(forecastDate);
     
         // to append current city weather
         var smain=document.createElement('h5');
-        smain.textContent='Weather Conditions: '+res.weather[0].main;
+        smain.textContent='Weather Conditions: '+res.list[i].weather[0].main;
         searchWeather.append(smain);
     
-        // to append current city temp
+//         // to append current city temp ==>Funtcioning properly now!!!
+
         var searchTemp=document.createElement('h5');
-        searchTemp.textContent='Temperature: '+Math.round((res.main.temp - 273.15) * (9/5) + 32)
+        searchTemp.textContent='Temperature: '+Math.round((res.list[i].main.temp - 273.15) * (9/5) + 32)
         searchWeather.append(searchTemp);
     
-        // to append current humidity
+//         // to append current humidity
         var shumidity=document.createElement('h5');
-        shumidity.textContent='Humidity: ' + res.main.humidity;
+        shumidity.textContent='Humidity: ' + res.list[i].main.humidity;
         searchWeather.append(shumidity);
     
-        // to append current windspeed
+//         // to append current windspeed
         var swindSpeed=document.createElement('h5');
-        swindSpeed.textContent='Wind Speed: '+ res.wind.speed;
-        searchWeather.append(swindSpeed);
+        swindSpeed.textContent='Wind Speed: '+ res.list[i].wind.speed;
+        searchWeather.append(swindSpeed); 
+}
+});
     
-})  
-
 };
 
 // creating onclick for weathersearch function/search button
