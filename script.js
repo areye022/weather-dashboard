835355f3e911865e6c45463c0648d41c57c4c2d2
areye022 <areye022@gmail.com>
2020-09-17 22:04:14 -0700

    reverted icons back to original, b/c new way was not functioning properly

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function locationRetrieved(position) {
     longitude = position.coords.longitude;  
 }
 
+var prevSearches=[];
 
 // function to search a specific city
 function weatherSearch(){
@@ -53,36 +54,28 @@ function weatherSearch(){
     searchCity.textContent=res.city.name;
     searchWeather.append(searchCity);
         console.log(res.list[i].weather[0].main);
-    //     // adding icons
-    var icon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + res.list[i].weather[0].icon + ".png");
-     // icon.addClass("image-styling")
-        searchWeather.append(icon);
-        // iconContainer.append(icon);
-    // if (res.list[i].weather[0].main==='Clouds'){
-    //     console.log('hi');
-    //     var iconContainer=$('<div>');
-        
-    //     // var icon=$('<img>').attr("src",'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ3bjRqzFjxkmvAWL3ff72-tH8UidJ4UxFzmQ&usqp=CAU' );
-    //    
-    // }
-    // else if(res.list[i].weather[0].main==='haze'){
-    //     $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ3bjRqzFjxkmvAWL3ff72-tH8UidJ4UxFzmQ&usqp=CAU' height=200px width:200px />");
-    // }
-    // else if(res.list[i].weather[0].main==='Clear'){
-    //     $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQB5rA_3-rOSGZygBtaYfgA6YdfW9pRFav4vQ&usqp=CAU' height=200px width:200px />");
-    // }
-    // else if(res.list[i].weather[0].main==='Rain'){
-    //     $("#image-div").html("<img src='https://lh3.googleusercontent.com/proxy/osGz8h9UkfY_pgBR60hlzNuBDfubrEzmOeipoz54xKwsrl900mZEQl8pru8LAWpLxpwlWaQUCJclxaj6oQZhkjA4Tx6RfGpXEJRwNV2Ug9PcAxFkZDmX7EOfyT5_t7UyhMTxSM8WgOLOfxc5tpf_adMnsp-D2KcocJyrFAk7V4_JFWQQPLnddvUKaRw' height=200px width:200px />");
-    // }
-    // else if(res.list[i].weather[0].main==='Thunderstorm'){
-    //     $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHUzZMG5T41Na5OYewYjGrMwy56nxcyl1vJQ&usqp=CAU' height=200px width:200px />");
-    // }
-    // else if(res.list[i].weather[0].main==='snow'){
-    //     $("#image-div").html("<img src='https://lh3.googleusercontent.com/proxy/2ZPXMzmsBUygSnCxTujchS45Ry6QCRfhbt_xPGOKdxOpcMs_AMkMBKtG3Zblqgalyxu-vKRilAeQPkp4JOEPTYGMR0343H2DoqGr-nQU-tHhbIcmkQDzOnM-uBGDF1BqFklABBYkSgt3KTy_Mh0aW8KeNV91uVW9CTB9j41dcy0B5wr8LIXm0on7G0VmiJpqaPlk9Q' height=200px width:200px />");
-    // }
+    
+    // adding icons
+    if (res.list[i].weather[0].main==='Clouds'){
+        $('#image-div').html("<img src=https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ3bjRqzFjxkmvAWL3ff72-tH8UidJ4UxFzmQ&usqp=CAU' height=200px width:200px />");
+    }
+    else if(res.list[i].weather[0].main==='haze'){
+        $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ3bjRqzFjxkmvAWL3ff72-tH8UidJ4UxFzmQ&usqp=CAU' height=200px width:200px />");
+    }
+    else if(res.list[i].weather[0].main==='Clear'){
+        $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQB5rA_3-rOSGZygBtaYfgA6YdfW9pRFav4vQ&usqp=CAU' height=200px width:200px />");
+    }
+    else if(res.list[i].weather[0].main==='Rain'){
+        $("#image-div").html("<img src='https://lh3.googleusercontent.com/proxy/osGz8h9UkfY_pgBR60hlzNuBDfubrEzmOeipoz54xKwsrl900mZEQl8pru8LAWpLxpwlWaQUCJclxaj6oQZhkjA4Tx6RfGpXEJRwNV2Ug9PcAxFkZDmX7EOfyT5_t7UyhMTxSM8WgOLOfxc5tpf_adMnsp-D2KcocJyrFAk7V4_JFWQQPLnddvUKaRw' height=200px width:200px />");
+    }
+    else if(res.list[i].weather[0].main==='Thunderstorm'){
+        $("#image-div").html("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHUzZMG5T41Na5OYewYjGrMwy56nxcyl1vJQ&usqp=CAU' height=200px width:200px />");
+    }
+    else if(res.list[i].weather[0].main==='snow'){
+        $("#image-div").html("<img src='https://lh3.googleusercontent.com/proxy/2ZPXMzmsBUygSnCxTujchS45Ry6QCRfhbt_xPGOKdxOpcMs_AMkMBKtG3Zblqgalyxu-vKRilAeQPkp4JOEPTYGMR0343H2DoqGr-nQU-tHhbIcmkQDzOnM-uBGDF1BqFklABBYkSgt3KTy_Mh0aW8KeNV91uVW9CTB9j41dcy0B5wr8LIXm0on7G0VmiJpqaPlk9Q' height=200px width:200px />");
+    }
 
         // to append date
-        // date doesn't come out correctly , might just use moment();
         var forecastDate = document.createElement("h5");
         var forecastnow =(new Date(res.list[i].dt_txt).toLocaleDateString());
         // updating the text in the html
@@ -116,6 +109,11 @@ function weatherSearch(){
 // creating onclick for weathersearch function/search button
 $('#searchBtn').on('click',function(event){
     event.preventDefault();
+    var searchWeather = document.querySelector('#searched-city')
+    var cityName = document.getElementById('keyTerm').value
+    localStorage.setItem('city', JSON.stringify(cityName));
+    prevSearches.push(cityName);
+    
     var searchWeather=document.querySelector('#searched-city')
     searchWeather.innerHTML="",
     weatherSearch();
